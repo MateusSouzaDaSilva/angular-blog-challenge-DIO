44361f7c87d68d944dad7db5ca7a2358fefda6ce
Mateus <mateus.ssilva2002@gmail.com>
2024-01-24 16:23:42 -0300

    content created, mocked data working

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "1",
+        "title": "A fantastica fabrica de chocolates",
+        "description": "Filme é previsto para estreiar em dezembro",
+        "photoCover": "https://live.staticflickr.com/7255/8161636190_e7be214ac6_z.jpg"
     },
     {
-        "id": 2,
-        "photo": "bbb",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "2",
+        "title": "Filme do Tintim estreia em breve",
+        "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, nulla eum officia totam fugit et ipsa expedita rem itaque quae, quasi facilis perferendis vero nostrum. Perferendis quae incidunt at iste.",
+        "photoCover": "https://2.bp.blogspot.com/-TY9jRNnFMw4/Tmfc8eNGDtI/AAAAAAAAGFo/PnqTFltxw_g/s1600/x-big+banner.jpg"
     },
 ]
